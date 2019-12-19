1de7de8431bccfd8cd8cea13257cc9258b296b90
Хван Дмитрий Максимович <drestbm@gmail.com>
2019-12-19 22:03:36 +0700

    first commit

---
+++ www/precache-manifest.87d893199161e983570d78110597ee1a.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4fa4d08352f2354dc6f783d98231fc8f",
+    "url": "./index.html"
+  },
+  {
+    "revision": "54e792aea8b01eb56210",
+    "url": "./static/css/main.94a7e78c.chunk.css"
+  },
+  {
+    "revision": "f8b9f333f35860fcba98",
+    "url": "./static/js/2.60065928.chunk.js"
+  },
+  {
+    "revision": "54e792aea8b01eb56210",
+    "url": "./static/js/main.d8c5fa2a.chunk.js"
+  },
+  {
+    "revision": "0bf1d0f451c06bb3f45b",
+    "url": "./static/js/runtime-main.3b8f5d0f.js"
+  },
+  {
+    "revision": "cbda35d523658ecfe220bb1a720de6d2",
+    "url": "./static/media/back.cbda35d5.svg"
+  },
+  {
+    "revision": "41cd842ac18413057134e0d775d6f006",
+    "url": "./static/media/no.41cd842a.svg"
+  },
+  {
+    "revision": "8f19d1d76e05f86c48a686e021cd8862",
+    "url": "./static/media/reload.8f19d1d7.svg"
+  },
+  {
+    "revision": "1defd08527eaa3d4f9f5e8427fe6d4b8",
+    "url": "./static/media/yes.1defd085.svg"
+  }
+]);
